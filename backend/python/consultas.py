54e79f576366088a29bc8d4cfbc71b3c97257e81
PereAPrats <ppratsv03@gmail.com>
2024-05-05 04:45:57 +0200

    Find users by similar dates and same location

--- backend/python/consultas.py
+++ backend/python/consultas.py
@@ -8,15 +8,18 @@ df['Departure Date'] = pd.to_datetime(df['Departure Date'], format="%d/%m/%Y")
 df['Return Date'] = pd.to_datetime(df['Return Date'], format="%d/%m/%Y")
 
 
-def getTravelersByMonth(date):
+def getTravelers(date, city):
     global df
     date = pd.to_datetime(date, format="%d/%m/%Y")
     
-    filtered = df[(date >= df['Departure Date']) & (date <= df['Return Date'])]
-    filtered['Departure Date'] = filtered['Departure Date'].astype(str)
-    filtered['Return Date'] = filtered['Return Date'].astype(str)
+    dateFiltered = df[(date >= df['Departure Date']) & (date <= df['Return Date'])]
+    dateFiltered.loc[:, 'Departure Date'] = dateFiltered['Departure Date'].astype(str)
+    dateFiltered.loc[:, 'Return Date'] = dateFiltered['Return Date'].astype(str)
     
-    return filtered.to_json(orient='records', lines=True)
+    locFilter = dateFiltered[city.lower() == dateFiltered['Arrival City'].str.lower()]
+    
+    return locFilter.to_json(orient='records', lines=True)
             
         
-    
+with open('travels.json', 'w') as file:
+    file.write(getTravelers('17/06/2024', 'Barcelona'))
